9147880ece751fab3dcefdada1a174dd6a5d9472
kamehouseorbust <hayden.russell@dreamsocket.com>
2019-01-14 22:48:22 -0500

    [CHG] BNR Android Ch. 20

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+
+    public SoundViewModel(BeatBox beatbox) {
+        mBeatBox = beatbox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
