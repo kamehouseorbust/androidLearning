f55342f571ca1892e74195631cff98f640d87ec3
kamehouseorbust <hayden.russell@dreamsocket.com>
2019-01-21 02:30:02 -0500

    Remove Api key

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -19,7 +19,7 @@ public class FlickrFetchr {
 
     private static final String TAG = "FlickrFetchr";
 
-    private static final String API_KEY = "b543c826011c6818723313690ad60e6a";
+    private static final String API_KEY = "";
     private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
     private static final String SEARCH_METHOD = "flickr.photos.search";
     private static final Uri ENDPOINT = Uri
